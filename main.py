61121a21e6eab54e8bfeceed0825fcfc56808ead
sgoutham24 <sgouthams024@gmail.com>
2025-03-08 11:13:26 +0530

    0.1.1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def swap_cases(s):
+    x = ""
+    for i in s:
+        if i.isupper():
+            x += i.lower()
+        else:
+            x += i.upper()
+    return x
 
+
+x = input("Enter string:")
+print(swap_cases(x))
